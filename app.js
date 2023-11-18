f92106663028c82444598225bab12e4fca5eee63
Jake Korobellis <jkorobellis@gmail.com>
2023-11-18 18:54:33 -0500

    sfd

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const router = require("./routes/index");
 var app = express();
 
 app.use(express.json());
 app.use("/", router);
+app.use(
+  cors({
+    origin:
+      "https://65594e1db025a846b273eaa4--poetic-hotteok-658b06.netlify.app/",
+    // Additional options if needed...
+  })
+);
 
 app.get("/api", (req, res) => {
   res.json({ test: true });
