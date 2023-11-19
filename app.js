32ba978d52d54bbc20758fbda5242af3868d3bd5
Jake Korobellis <jkorobellis@gmail.com>
2023-11-18 20:16:59 -0500

    re

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ require("dotenv").config();
 const router = require("./routes/index");
 var app = express();
 
+mongoose.set("strictQuery", false);
+
+const DB = process.env.DBLINK;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(DB);
+  console.log("MongoDB is connected");
+}
+
 app.use(cors());
 app.use(express.json());
 app.use("/", (req, res) => {
@@ -16,13 +26,3 @@ console.log("Cors connected ");
 app.get("/api", (req, res) => {
   res.json({ test: true });
 });
-
-mongoose.set("strictQuery", false);
-
-const DB = process.env.DBLINK;
-
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(DB);
-  console.log("MongoDB is connected");
-}
