f7ee3d7020d1faf891feb150f6b5271402f1daf6
Jake Korobellis <jkorobellis@gmail.com>
2023-11-18 21:00:10 -0500

    wertrytrhger

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const router = require("./routes/index");
-var app = express();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  // router
+  res.send("Hello world");
+});
+
+console.log("Cors connected ");
+
+app.get("/api", (req, res) => {
+  res.json({ test: true });
+});
 
 mongoose.set("strictQuery", false);
 
@@ -15,15 +31,4 @@ async function main() {
   console.log("MongoDB is connected");
 }
 
-app.use(cors());
-app.use(express.json());
-app.use("/", (req, res) => {
-  // router
-  res.send("Hello world");
-});
-console.log("Cors connected ");
-
-app.get("/api", (req, res) => {
-  res.json({ test: true });
-});
-app.listen(5000, () => console.log("App is listening on port 5000"));
+app.listen(port, () => console.log("App is listening on port 5000"));
